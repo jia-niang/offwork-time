be2ffaa4c9b139d6ea82abcdac193c5033d1f980
FrankPu <96pulf@gmail.com>
2021-11-04 19:41:02 +0800

    fix(components/datepicker): fix Omit

--- src/components/DateTimePicker/TimePicker.tsx
+++ src/components/DateTimePicker/TimePicker.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import { PickerTimeProps } from 'antd/es/date-picker/generatePicker'
-import { Omit } from 'antd/es/_util/type'
 
 import DatePicker from './DatePicker'
 
