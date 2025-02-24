1f34aa16b3e7f49f2771799ae626f56707e946e9
jia-niang <1@paperplane.cc>
2025-02-24 10:51:23 +0800

    chore(app): 现在使用 pnpm；现在会在 CI/CD 步骤进行上传 CDN；现在兼容 node 新版本；锁定依赖项的版本号

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # offworkhours
-
-CI TEST
--- config-overrides.js
+++ config-overrides.js
@@ -6,7 +6,6 @@ const {
   addBabelPlugins,
   addWebpackPlugin,
 } = require('customize-cra')
-const S3Plugin = require('webpack-s3-plugin')
 const HtmlWebpackTagsPlugin = require('html-webpack-tags-plugin')
 const noop = require('lodash/identity')
 
@@ -39,26 +38,7 @@ module.exports = {
       })
     ),
 
-    process.env.NODE_ENV === 'production' && process.env.COS_SECRET_ID && process.env.COS_SECRET_KEY
-      ? addWebpackPlugin(
-          new S3Plugin({
-            exclude: /.*\.html$/,
-            basePath: 'paperplane-offwork-time',
-            s3Options: {
-              accessKeyId: process.env.COS_SECRET_ID,
-              secretAccessKey: process.env.COS_SECRET_KEY,
-              region: 'ap-hongkong',
-              endpoint: 'https://cos.ap-hongkong.myqcloud.com',
-              apiVersion: '2006-03-01',
-            },
-            s3UploadOptions: {
-              Bucket: 'paperplane-cdn-1253277322',
-            },
-          })
-        )
-      : noop,
-
-    process.env.NODE_ENV === 'production' && process.env.COS_SECRET_ID && process.env.COS_SECRET_KEY
+    process.env.NODE_ENV === 'production'
       ? function setPublicPath(config) {
           config.output.publicPath = '//cdn.paperplane.cc/paperplane-offwork-time/'
 
